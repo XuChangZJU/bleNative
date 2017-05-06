a857bb3f8f72159a1e99b59e6c0226ed0b7157bb
lavystord <lavystord@gmail.com>
2017-05-06 19:57:56 +0800

    upgrade rn for 0.40

--- ios/BleNative/BleNative.h
+++ ios/BleNative/BleNative.h
@@ -1,12 +1,10 @@
-//
-//  BleNative.h
-//  BleNative
-//
-//  Created by biliyuan on 16/4/25.
-//  Copyright © 2016年 biliyuan. All rights reserved.
-//
 
+#if __has_include(<React/RCTBridgeModule.h>)
+#import <React/RCTBridgeModule.h>
+#else // back compatibility for RN version < 0.40
 #import "RCTBridgeModule.h"
+#endif
+
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "BleConstantsConverter.h"
 
