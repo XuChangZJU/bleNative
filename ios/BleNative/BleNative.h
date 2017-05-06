4836ae1fa4ceba2f43e89b88f7fbc92fb0a5ea78
lavystord <lavystord@gmail.com>
2017-05-06 22:56:29 +0800

    upgrade step2

--- ios/BleNative/BleNative.h
+++ ios/BleNative/BleNative.h
@@ -1,9 +1,16 @@
 
 #if __has_include(<React/RCTBridgeModule.h>)
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventDispatcher.h>
+#import <React/RCTLog.h>
+#import <React/RCTConvert.h>
 #else // back compatibility for RN version < 0.40
 #import "RCTBridgeModule.h"
+#import "RCTEventDispatcher.h"
+#import "RCTLog.h"
+#import "RCTConvert.h"
 #endif
+
 
 #import <CoreBluetooth/CoreBluetooth.h>
 #import "BleConstantsConverter.h"
